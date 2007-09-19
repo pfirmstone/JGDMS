183bdeaecd022d85acabc9128ae64db465869a7e
Frank Barnaby <fbarnaby@apache.org>
2007-09-19 19:07:03 +0000

    Moved ServiceUI sources to the JSTK tree. [JIRA issue https://issues.apache.org/jira/browse/RIVER-264]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/river/trunk/jtsk@577416 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/artima/lookup/util/ConsistentSet.java
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.artima.lookup.util;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.io.Serializable;
+
+/**
+ * An implementation of the <code>java.util.Set</code> interface that has
+ * a serialized form consistent in all virtual machines. <code>ConsistentSet</code>
+ * instances are unmodifiable. All mutator methods, such as <code>add</code> and
+ * <code>remove</code>, throw <code>UnsupportedOperationException</code>.
+ * This class permits the <code>null</code> element.
+ *
+ * <p>
+ * Although instances of this class are unmodifiable, they are not necessarily
+ * immutable. If a client retrieves a mutable object contained in a
+ * <code>ConsistentSet</code> and mutates that object, the client in effect
+ * mutates the state of the <code>ConsistentSet</code>. In this case, the
+ * serialized form of the <code>ConsistentSet</code> will also most likely
+ * have been mutated. A <code>ConsistentSet</code> that contains only immutable
+ * objects will maintain a consistent serialized form indefinitely. But a 
+ * <code>ConsistentSet</code> that contains mutable objects will maintain a
+ * consistent serialized form only so long as the mutable objects are not
+ * mutated.
+ *
+ * @author Bill Venners
+ */
+public class ConsistentSet extends AbstractSet implements Serializable {
+
+    private static final long serialVersionUID = -533615203387369436L;
+
+    /**
+     * @serial An array of the <code>Object</code> elements contained in
+     *     this <code>ConsistentSet</code>.
+     */
+    private Object[] elements;
+
+    /**
+     * Constructs a new, empty <code>ConsistentSet</code>. All instances
+     * of <code>ConsistentSet</code> are unmodifiable.
+     */
+    public ConsistentSet() {
+        elements = new Object[0];
+    }
+
+    /**
+     * Constructs a new <code>ConsistentSet</code> containing the elements
+     * in the passed collection. All instances of <code>ConsistentSet</code>
+     * are unmodifiable.
+     *
+     * @param init the collection whose elements are to be placed into this set.
+     * @throw NullPointerException if the passed <code>init</code> reference
+     *     is <code>null</code>
+     */
+    public ConsistentSet(Collection init) {
+
+        if (init == null) {
+            throw new NullPointerException();
+        }
+
+        // Put the collection in a HashSet to get rid of duplicates
+        HashSet tempSet = new HashSet(init);
+
+        elements = new Object[tempSet.size()];
+
+        Iterator it = tempSet.iterator();
+        for (int i = 0; it.hasNext(); ++i) {
+            elements[i] = it.next();
+        }
+    }
+
+    /**
+     * Returns an <code>iterator</code> over the elements in this set. The elements
+     * are returned in no particular order. Because all instances of
+     * <code>ConsistentSet</code> are unmodifiable, the <code>remove</code> method 
+     * of the returned <code>Iterator</code> throws
+     * <code>UnsupportedOperationException</code>.
+     *
+     * @return an <code>Iterator</code> over the elements in this
+     *     <code>ConsistentSet</code>.
+     */
+    public Iterator iterator() {
+
+        return new Iterator() {
+
+            private int nextPos = 0;
+
+            public boolean hasNext() {
+                return nextPos < elements.length;
+            }
+
+            public Object next() {
+                Object next = elements[nextPos];
+                ++nextPos;
+                return next;
+            }
+
+            public void remove() {
+                throw new IllegalArgumentException();
+            }
+        };
+    }
+
+    /**
+     * Returns the number of elements in this <code>ConsistentSet</code> (its cardinality).
+     *
+     * @return the number of elements in this <code>ConsistentSet</code> (its cardinality).
+     */
+    public int size() {
+        return elements.length;
+    }
+}
+
---
+++ src/net/jini/lookup/entry/UIDescriptorBean.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.jini.lookup.entry;
+
+import java.io.Serializable;
+import net.jini.core.entry.Entry;
+import java.util.Set;
+import java.rmi.MarshalledObject;
+
+/**
+ * A JavaBeans(TM) component that encapsulates a <code>UIDescriptor</code> object.
+ *
+ * @see UIDescriptor
+ */
+public class UIDescriptorBean implements EntryBean, Serializable {
+
+    private static final long serialVersionUID = -3185268510626360709L;
+    
+    /**
+     * @serial The <code>UIDescriptor</code> object associated with this JavaBeans component.
+     */
+    protected UIDescriptor assoc;
+
+    /**
+     * Construct a new JavaBeans component, linked to a new empty <code>UIDescriptor</code> object.
+     */
+    public UIDescriptorBean() {
+        assoc = new UIDescriptor();
+    }
+
+    /**
+     * Make a link to a <code>UIDescriptor</code> object.
+     *
+     * @param e the <code>Entry</code> object, which must be a <code>UIDescriptor</code>, to which to link
+     * @exception java.lang.ClassCastException the <code>Entry</code> is not 
+     *     a <code>UIDescriptor</code>, the correct type for this JavaBeans component
+     */
+    public void makeLink(Entry e) {
+        assoc = (UIDescriptor) e;
+    }
+
+    /**
+     * Return the <code>UIDescriptor</code> linked to by this JavaBeans component.
+     */
+    public Entry followLink() {
+        return assoc;
+    }
+
+    /**
+     * Return the value of the <code>role</code> field in the object linked to by
+     * this JavaBeans component.
+     *
+     * @return a <code>String</code> representing the role value
+     * @see #setRole
+     */
+    public String getRole() {
+        return assoc.role;
+    }
+
+    /**
+     * Set the value of the <code>role</code> field in the object linked to by this
+     * JavaBeans component.
+     *
+     * @param role  a <code>String</code> specifying the role value
+     * @see #getRole
+     */
+    public void setRole(String role) {
+        assoc.role = role;
+    }
+
+    /**
+     * Return the value of the <code>toolkit</code> field in the object linked to by
+     * this JavaBeans component.
+     *
+     * @return a <code>String</code> representing the toolkit value
+     * @see #setToolkit
+     */
+    public String getToolkit() {
+        return assoc.toolkit;
+    }
+
+    /**
+     * Set the value of the <code>toolkit</code> field in the object linked to by this
+     * JavaBeans component.
+     *
+     * @param toolkit  a <code>String</code> specifying the toolkit value
+     * @see #getToolkit
+     */
+    public void setToolkit(String toolkit) {
+        assoc.toolkit = toolkit;
+    }
+
+    /**
+     * Return the value of the <code>attributes</code> field in the object linked to by
+     * this JavaBeans component.
+     *
+     * @return a <code>Set</code> representing the attributes value
+     * @see #setAttributes
+     */
+    public Set getAttributes() {
+        return assoc.attributes;
+    }
+
+    /**
+     * Set the value of the <code>attributes</code> field in the object linked to by this
+     * JavaBeans component.
+     *
+     * @param attributes  a <code>Set</code> specifying the attributes value
+     * @see #getAttributes
+     */
+    public void setAttributes(Set attributes) {
+        assoc.attributes = attributes;
+    }
+
+    /**
+     * Return the value of the <code>factory</code> field in the object linked to by
+     * this JavaBeans component.
+     *
+     * @return a <code>MarshalledObject</code> representing the factory value
+     * @see #setFactory
+     */
+    public MarshalledObject getFactory() {
+        return assoc.factory;
+    }
+
+    /**
+     * Set the value of the <code>factory</code> field in the object linked to by this
+     * JavaBeans component.
+     *
+     * @param factory  a <code>MarshalledObject</code> specifying the factory value
+     * @see #getFactory
+     */
+    public void setFactory(MarshalledObject factory) {
+        assoc.factory = factory;
+    }
+}
