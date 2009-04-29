5a3cb5cb2cd7ce92a1d64eabe85ea6dc6afb15ce
Tom Hobbs <thobbs@apache.org>
2009-04-29 19:59:44 +0000

    Placeholder for unit test for River-283. Also trying to validate I have my SVNKit setup properly in eclipse.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/river/jtsk/trunk@769903 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/com/sun/jini/outrigger/TxnTableTest.java
@@ -0,0 +1,16 @@
+package com.sun.jini.outrigger;
+
+import junit.framework.TestCase;
+
+public class TxnTableTest extends TestCase {
+
+	/**
+	 * Test to expose the bug identified in River-
+	 * Test to validate patch supplied in River-283
+	 * 
+	 */
+	public void testKeyEquals() {
+		fail("No test implementation yet");
+	}
+	
+}
