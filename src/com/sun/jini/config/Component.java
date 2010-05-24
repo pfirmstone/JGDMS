abd36d0b7ed27f7de96bf2d7583db6232947dd87
Peter Firmstone <peter_firmstone@apache.org>
2010-05-24 10:04:46 +0000

    River-313 Groovy Config donated by Dennis Reedy, change location to com.sun.config, will be moved later to org.apache.river later with com.sun.config package.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/river/jtsk/trunk@947584 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/sun/jini/config/Component.java
+++ src/com/sun/jini/config/Component.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.jini.config;
+package com.sun.jini.config;
 
 import java.lang.annotation.*;
 
