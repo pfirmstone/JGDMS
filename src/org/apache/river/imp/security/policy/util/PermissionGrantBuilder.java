1039b43701b00557b8cac62a5ffb7e2e8c0b89de
Peter Firmstone <peter_firmstone@apache.org>
2010-06-24 02:59:43 +0000

    River-340 Further Refactoring
    git-svn-id: https://svn.apache.org/repos/asf/incubator/river/jtsk/trunk@957405 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/river/imp/security/policy/util/PermissionGrantBuilder.java
+++ src/org/apache/river/imp/security/policy/util/PermissionGrantBuilder.java
@@ -69,7 +69,19 @@ public class PermissionGrantBuilder {
     
     /**
      * This method set's the Context of the applied Grant, that is if a 
-     * Permission is to be granted directly to a PermissionDomain
+     * Permission is to be granted directly to a ProtectionDomain, ClassLoader
+     * (which may encompass many ProtectionDomain's), CodeSource ( which may
+     * again apply to multiple ProtectionDomains) and Certificate grant's which
+     * may apply to CodeSource's that have been signed by a particular set of 
+     * signers.  In all cases grants only apply where a ProtectionDomain
+     * satisfies a specific set of Principal's specified in the PermissionGrant.
+     * 
+     * Null Principals means the Permission's apply to any Principal.
+     * 
+     * Null Class means null ClassLoader or ProtectionDomain which results in
+     * the Permission's being granted system wide to all ProtectionDomains,
+     * provided those ProtectionDomain's satsify the set of Principal's.
+     * 
      * @param context
      * @return
      */
@@ -84,32 +96,77 @@ public class PermissionGrantBuilder {
         return this;
     }
     
+    /**
+     * Set the CodeSource for the PermissionGrant
+     * @param cs
+     * @return
+     */
     public PermissionGrantBuilder codeSource(CodeSource cs){
         this.cs = cs;
         return this;
     }
     
+    /**
+     * The Class passed in will be utilised to determine the ClassLoader,
+     * ProtectionDomain, CodeSource or Certificates relating to that class
+     * as determined by the context, provided the builders internal fields are 
+     * Null - have not be set by builder methods.
+     * 
+     * This will also set the Principals to that of class's Principal's
+     * providing of course the Principals have not already been set.
+     * 
+     * @param cl
+     * @return
+     */
     public PermissionGrantBuilder clazz(Class cl){        
         domain = new WeakReference<ProtectionDomain>(cl.getProtectionDomain());
+        if (cs == null){
+            cs = domain.get().getCodeSource();
+        }
+        if (certs == null){
+            certs = domain.get().getCodeSource().getCertificates();
+        }
+        if (principals == null){
+            principals = domain.get().getPrincipals();
+        }
         return this;
     }
     
+    /**
+     * Set the Certificate's for the PermissionGrant.
+     * @param certs
+     * @return
+     */
     public PermissionGrantBuilder certificates(Certificate[] certs){
         this.certs = certs;
         return this;
     }
     
+    /**
+     * Set the Principal's for the PermissionGrant.
+     * @param pals
+     * @return
+     */
     public PermissionGrantBuilder principals(Principal[] pals){
         // don't worry about being protective here.
         this.principals = pals;
         return this;
     }
     
+    /**
+     * Set the Permission's for the PermissionGrant.
+     * @param permissions
+     * @return
+     */
     public PermissionGrantBuilder permissions(Permission[] permissions){
         this.permissions = permissions;
         return this;
     }
     
+    /**
+     * Build the PermissionGrant using information supplied.
+     * @return an appropriate PermissionGrant.
+     */
     public PermissionGrant build(){
         switch (context){
             case CLASSLOADER:
