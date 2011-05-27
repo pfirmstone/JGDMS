9825dd592a5ab4b5dda8b6d6d4d3c00ed621ab5e
Tom Hobbs <thobbs@apache.org>
2011-05-27 10:37:00 +0000

    committing merge
    git-svn-id: https://svn.apache.org/repos/asf/river/jtsk/branches/2.2@1128245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rat_reports.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "${RAT_HOME+x}" = "x" ]; then
+	echo "Using RAT_HOME=${RAT_HOME}"
+else
+	echo "Please set RAT_HOME before continuing"
+	exit 1
+fi
+
+java -jar $RAT_HOME/apache-rat-0.7.jar -d src > RAT_REPORT_src.txt
+java -jar $RAT_HOME/apache-rat-0.7.jar -d examples -e *.mf > RAT_REPORT_examples.txt
+java -jar $RAT_HOME/apache-rat-0.7.jar -d qa/jtreg -e *.mf > RAT_REPORT_qa_jtreg.txt
