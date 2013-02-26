4511c8d8500510bd4c7cb4937802b4424304897f
Peter Firmstone <peter_firmstone@apache.org>
2013-02-26 13:39:21 +0000

    Fix OOM error.
    git-svn-id: https://svn.apache.org/repos/asf/river/jtsk/skunk/qa_refactor/trunk@1450167 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/river/api/security/PermissionGrant.java
+++ src/org/apache/river/api/security/PermissionGrant.java
@@ -165,7 +165,6 @@ public abstract class PermissionGrant {
      * @return true - if PermissionGrant contains AllPermission.
      */
     public final boolean isPrivileged(){
-        if (decorated != null) return decorated().isPrivileged();
         return privileged;
     }
 
