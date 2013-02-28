787edce99ad933cd2298e155281456c8f53228d8
Peter Firmstone <peter_firmstone@apache.org>
2013-02-28 08:17:50 +0000

    RIVER-416
    Added fail safe mechanism in case of future Level serial form breakage based on suggestion made by Robert Gibson that only uses public API.
    
    git-svn-id: https://svn.apache.org/repos/asf/river/jtsk/skunk/qa_refactor/trunk@1451135 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/sun/jini/logging/Levels.java
+++ src/com/sun/jini/logging/Levels.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamClass;
+import java.io.ObjectStreamException;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.logging.Level;
@@ -115,7 +116,16 @@ public class Levels {
     * Creates an instance of the Level class. This method works around the
     * fact that there is no public constructor for the Level class by
     * constructing the serialized form for an instance with the specified
-    * field values and deserializing it.
+    * field values and deserializing it. 
+    * 
+    * If deserialization fails, it creates a Level with a numerical name that 
+    * can be de-serialised by a remote client that doesn't have
+    * com.sun.jini.logging.Levels bytecode.
+    * 
+    * Local logging code still enjoys the benefit of a meaningful name even
+    * when deserialization fails.
+    * 
+    * See River-416 for details, the serial form of Levels was broken in Java 1.6.0_41.
     */
     private static Level createLevel(String name, int value, String resourceBundleName) {
         try {
@@ -126,9 +136,17 @@ public class Levels {
             ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes.toByteArray()));
             Level result = (Level) in.readObject();
             in.close();
+            // If this suceeds, Level.readResolve has added the new Level to its internal List.
             return result;
         } catch (Exception e) {
-            throw new RuntimeException("Unexpected exception", e);
+            final Level withoutName = Level.parse(Integer.valueOf(value).toString());
+            Level l =  new Level(name, value, resourceBundleName) {
+                Object writeReplace() throws ObjectStreamException {
+                    return withoutName;
+                }
+            };
+            return l;
         }
+        
     }
 }
