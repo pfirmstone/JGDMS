e64cafa57ba668e5cfd7f972a446c2a901b151fc
Greg Trasuk <gtrasuk@apache.org>
2013-08-08 03:58:21 +0000

    Fix RIVER-424. Add pom file for jsk-policy, and add jsk-policy to the deploy_river.groovy script.
    git-svn-id: https://svn.apache.org/repos/asf/river/jtsk/branches/2.2@1511568 13f79535-47bb-0310-9956-ffa450edef68

--- poms/deploy_river.groovy
+++ poms/deploy_river.groovy
@@ -16,11 +16,16 @@
  */
 String version = "2.2.1"
 String rootDir = ".."
+String repositoryId="apache.releases.https"
+String repositoryURL="https://repository.apache.org/service/local/staging/deploy/maven2"
+//String repositoryURL="file:///Users/trasukg/mvn-repo"
+String passphrase="Insert passphrase here..."
         
 ["net.jini:jsk-platform":"lib",
  "net.jini:jsk-lib":"lib",
  "net.jini:jsk-dl":"lib-dl",
  "net.jini:jsk-resources":"lib",
+ "net.jini:jsk-policy":"lib",
  "net.jini.lookup:serviceui":"lib",
  "org.apache.river:fiddler":"lib",
  "org.apache.river:fiddler-dl":"lib-dl",
@@ -34,13 +39,14 @@ String rootDir = ".."
  "org.apache.river:outrigger-dl":"lib-dl",
  "org.apache.river:reggie":"lib",
  "org.apache.river:reggie-dl":"lib-dl",
- "org.apache.river:start":"lib"].each {artifact, subDir ->
+ "org.apache.river:start":"lib"
+].each {artifact, subDir ->
     
     String[] parts = artifact.split(":")
     String gId = parts[0]
     String aId = parts[1]
     String dir = rootDir+"/"+subDir
-    String deployCommand = "mvn deploy:deploy-file "+
+    /*String deployCommand = "mvn deploy:deploy-file "+
                            "-DrepositoryId=apache.releases.https "+
                            "-Dversion=${version} "+
                            "-DgeneratePom=false -Dpackaging=jar "+
@@ -49,6 +55,38 @@ String rootDir = ".."
                            "-Dfile=${dir}/${aId}.jar "+
                            "-DpomFile=./${aId}.pom "+
                            "-Durl=https://repository.apache.org/service/local/staging/deploy/maven2"
+    */
+    def deployCommand = [\
+        "mvn",
+        "gpg:sign-and-deploy-file",
+        "-DpomFile=${aId}.pom",
+        "-Dfile=${dir}/${aId}.jar",
+        "-DrepositoryId=${repositoryId}",
+	"-Durl=${repositoryURL}",
+        "-Dgpg.useAgent=false",
+	"-Dgpg.keyname=gtrasuk@apache.org",
+        "-Dgpg.passphrase=${passphrase}"
+    ]
+    /*
+	
+        /*"-Drepository=${repository} " +
+	
+	
+    */
+/*
+"mvn gpg:sign-and-deploy-file " +
+	"-Dpassphrase='situation business offensive persion want comprehension product features' " +
+	"-DpomFile=./${aId}.pom " +
+	"-Dfile=${dir}/${aId}.jar " + 
+	"-DrepositoryId=${repository} " +
+	"-Durl=${repositoryURL}"
+*/
+    /* First make sure the jar file contains the LICENSE file */
+    def p="jar uvf ${dir}/${aId}.jar -C .. LICENSE".execute()
+    p.consumeProcessOutputStream(System.out)
+    p.consumeProcessErrorStream(System.err)
+    p.waitFor()
+    
     println deployCommand
     Process process = deployCommand.execute()
     process.consumeProcessOutputStream(System.out)
