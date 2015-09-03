8544a0e4585ec6ec355beefb69fd3205db445262
Dennis Reedy <dreedy@apache.org>
2015-09-03 19:27:22 +0000

    Working towards release, adding groovy-config.jar support
    git-svn-id: https://svn.apache.org/repos/asf/river/jtsk/skunk/qa-refactor-namespace/trunk@1701112 13f79535-47bb-0310-9956-ffa450edef68

--- poms/deploy_river.groovy
+++ poms/deploy_river.groovy
@@ -55,3 +55,77 @@ String rootDir = ".."
     process.consumeProcessErrorStream(System.err)
     process.waitFor()
 }
+
+#!/usr/bin/env groovy
+/*
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+String version = "3.0
+String rootDir = ".."
+String repositoryId="apache.releases.https"
+String repositoryURL="https://repository.apache.org/service/local/staging/deploy/maven2"
+//String repositoryURL="file:///Users/trasukg/mvn-repo"
+String passphrase="Insert passphrase here..."
+        
+["net.jini:jsk-platform":"lib",
+ "net.jini:jsk-lib":"lib",
+ "net.jini:jsk-dl":"lib-dl",
+ "net.jini:jsk-resources":"lib",
+ "net.jini:jsk-policy":"lib-ext",
+ "net.jini.lookup:serviceui":"lib",
+ "org.apache.river:fiddler":"lib",
+ "org.apache.river:fiddler-dl":"lib-dl",
+ "org.apache.river:mahalo":"lib",
+ "org.apache.river:mahalo-dl":"lib-dl",
+ "org.apache.river:mercury":"lib",
+ "org.apache.river:mercury-dl":"lib-dl",
+ "org.apache.river:norm":"lib",
+ "org.apache.river:norm-dl":"lib-dl",
+ "org.apache.river:outrigger":"lib",
+ "org.apache.river:outrigger-dl":"lib-dl",
+ "org.apache.river:reggie":"lib",
+ "org.apache.river:reggie-dl":"lib-dl",
+ "org.apache.river:start":"lib"
+ "org.apache.river:tools":"lib"
+].each {artifact, subDir ->
+    
+    String[] parts = artifact.split(":")
+    String gId = parts[0]
+    String aId = parts[1]
+    String dir = rootDir+"/"+subDir
+
+    def deployCommand = [\
+"mvn",
+"gpg:sign-and-deploy-file",
+"-DpomFile=${aId}.pom",
+"-Dfile=${dir}/${aId}.jar",
+"-DrepositoryId=${repositoryId}",
+"-Durl=${repositoryURL}",
+"-Dgpg.useAgent=false",
+"-Dgpg.keyname=gtrasuk@apache.org",
+"-Dgpg.passphrase=${passphrase}"]
+
+    /* First make sure the jar file contains the LICENSE file */
+    def p="jar uvf ${dir}/${aId}.jar -C .. LICENSE".execute()
+    p.consumeProcessOutputStream(System.out)
+    p.consumeProcessErrorStream(System.err)
+    p.waitFor()
+    
+    println deployCommand
+    Process process = deployCommand.execute()
+    process.consumeProcessOutputStream(System.out)
+    process.consumeProcessErrorStream(System.err)
+    process.waitFor()
+}
